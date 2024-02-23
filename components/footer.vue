f09d98b6ab03d1daf993a84f1f40582f8e4a3208
dludemann <dludemann@aspiralabs.co>
2024-02-23 15:27:02 -0500

    swap footer

--- components/footer.vue
+++ components/footer.vue
@@ -213,6 +213,13 @@ const nav_links =
             method="post"
           >
             <input
+              type="text"
+              name="fields[first_name]"
+              placeholder="First Name"
+              required
+              class="bg-white border border-[#D0D5DD] flex gap-2 py-2.5 px-[14px] rounded-lg font-body text-body-regular placeholder:text-[#667085] text-[#667085] items-center"
+            />
+            <input
               type="email"
               name="fields[email]"
               value=""
@@ -221,13 +228,6 @@ const nav_links =
               class="bg-white border border-[#D0D5DD] flex gap-2 py-2.5 px-[14px] rounded-lg font-body text-body-regular placeholder:text-[#667085] text-[#667085] items-center"
             />
 
-            <input
-              type="text"
-              name="fields[first_name]"
-              placeholder="First Name"
-              required
-              class="bg-white border border-[#D0D5DD] flex gap-2 py-2.5 px-[14px] rounded-lg font-body text-body-regular placeholder:text-[#667085] text-[#667085] items-center"
-            />
             <button
               class="px-6 py-3 flex gap-2 font-display text-lg leading-[25px] font-semibold justify-center bg-primary-900 text-white hover:bg-primary-700 focus:bg-primary-700 disabled:bg-secondary-100 transition duration-150 lg:w-auto"
               type="submit"
