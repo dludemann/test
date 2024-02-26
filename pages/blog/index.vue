8b19454ff96ae06015ffe0897bcf533522f4d63b
dludemann <dludemann@aspiralabs.co>
2024-02-25 23:53:57 -0500

    merge prod down

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,29 +2,30 @@
   <BlogLayout
     :posts="pagedPosts"
     :pageNumber="pageNumber"
-    :numberOfPages="numberOfPages" />
+    :numberOfPages="numberOfPages"
+  />
 </template>
 
 <script setup>
-definePageMeta({ layout: 'blog' });
+definePageMeta({ layout: "blog" });
 
 const pageNumber = 1;
 
-const pageData = await queryContent('blog').where({ _path: '/blog' }).findOne();
+const pageData = await queryContent("blog").where({ _path: "/blog" }).findOne();
 const pageSize = pageData.pagination.size || 9;
 
-const allPosts = await queryContent('blog')
-  .where({ _path: { $ne: '/blog' } })
-  .only(['title'])
+const allPosts = await queryContent("blog")
+  .where({ _path: { $ne: "/blog" } })
+  .only(["title"])
   .find();
 const numberOfPosts = allPosts.length;
 const numberOfPages = Math.ceil(numberOfPosts / pageSize);
 
-const pagedPosts = await queryContent('blog')
+const pagedPosts = await queryContent("blog")
   .where({
-    _path: { $ne: '/blog' },
+    _path: { $ne: "/blog" },
   })
-  .sort({ createdAt: 'asc' })
+  .sort({ createdAt: "asc" })
   .limit(pageData.pagination.size)
   .find();
 </script>
