20243be5cb81dc9fa7e6d07e1e8e6b6908583a37
dludemann <dludemann@aspiralabs.co>
2024-03-09 16:52:01 -0500

    booking page

---
+++ components/Booking.vue
@@ -0,0 +1,28 @@
+<script setup>
+const BOOKING_LINK =
+  "https://bookus.page/thematchartist/thematchartist/lite/free-consultation-with-shane";
+</script>
+
+<template>
+  <section
+    style="position: relative; padding-top: 100%"
+    class="hidden lg:block"
+  >
+    <iframe
+      :src="BOOKING_LINK"
+      frameborder="0"
+      style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+    />
+  </section>
+
+  <section
+    style="position: relative; padding-top: 200%"
+    class="block lg:hidden"
+  >
+    <iframe
+      :src="BOOKING_LINK"
+      frameborder="0"
+      style="position: absolute; top: 0; left: 0; width: 100%; height: 200%"
+    />
+  </section>
+</template>
