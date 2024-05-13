b514154d5b82af1504029d01da275c3f01ffb952
dludemann <dludemann@aspiralabs.co>
2024-05-13 15:27:47 -0400

    staging fixed random html characters

--- pages/book-a-call/index.vue
+++ pages/book-a-call/index.vue
@@ -6,6 +6,18 @@ useHead({
 </script>
 
 <template>
-  <Booking />
-  <ConnectWithUs />
+  <div className="bg-[#f6f8fa] ">
+    <div
+      className="bg-[#f6f8fa] p-8 max-w-[840px] mx-auto w-full text-center pt-8"
+    >
+      <p>
+        This is a free strategy session with one of our team members. We will
+        discuss your dating goals and explain how we optimize profiles for
+        maximum success. This call will also give you a chance to ask any
+        questions you have about The Match Artist and our process.
+      </p>
+    </div>
+    <Booking />
+    <ConnectWithUs />
+  </div>
 </template>
