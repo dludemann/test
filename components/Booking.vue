42eef335ac6a8b2a487430a61f271759581d3499
dludemann <dludemann@aspiralabs.co>
2024-05-14 01:25:39 -0400

    fix bookings page

--- components/Booking.vue
+++ components/Booking.vue
@@ -22,7 +22,7 @@ const BOOKING_LINK =
     <iframe
       :src="BOOKING_LINK"
       frameborder="0"
-      style="position: absolute; top: 0; left: 0; width: 100%; height: 200%"
+      style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
     />
   </section>
 </template>
--- pages/book-a-call/index.vue
+++ pages/book-a-call/index.vue
@@ -10,7 +10,7 @@ useHead({
     <div
       class="bg-[#f6f8fa] p-8 lg:max-w-[840px] mx-auto w-full text-center pt-8"
     >
-      <h1 class="text-2xl font-bold mb-4">Free Stragegy Session</h1>
+      <h1 class="text-2xl font-bold mb-4">Free Strategy Session</h1>
 
       <p>
         This is a free strategy session with one of our team members. We will
