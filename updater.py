b55ea9a6c49e83fe9ea799743412d7c2e4710d7e
dludemann <dludemann@aspiralabs.co>
2024-05-18 02:41:50 -0400

    update script

--- updater.py
+++ updater.py
@@ -1,31 +1,22 @@
 import os
 import re
 
-def replace_description_block(folder_path):
+def replace_image_block(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith(".md"):
             file_path = os.path.join(folder_path, filename)
             with open(file_path, 'r') as file:
                 content = file.read()
 
-            # Pattern to capture and replace the images block
-            images_pattern = re.compile(r"images:\s*\n((\s*- /images/.*\n)+)(?!\s*- )", re.MULTILINE)
-            new_images_block = """
-images:
-      - https://photostma.blob.core.windows.net/marketing/tejesh-tma-0033.jpeg
-      - https://photostma.blob.core.windows.net/marketing/jeff.jpeg
-      - https://photostma.blob.core.windows.net/marketing/justen-tma-9079.jpeg
-      - https://photostma.blob.core.windows.net/marketing/DSCF7389222.jpeg
-      - https://photostma.blob.core.windows.net/marketing/rohith-tma-2-191.jpeg
-      - https://photostma.blob.core.windows.net/marketing/connor-tma-210.jpeg
-      - https://photostma.blob.core.windows.net/marketing/DSCF5511.jpeg
-      - https://photostma.blob.core.windows.net/marketing/DSCF4069.jpeg
-      - https://photostma.blob.core.windows.net/marketing/ian-tma-0396.jpeg
-      - https://photostma.blob.core.windows.net/marketing/shakked-tma-06.jpeg
-      - https://photostma.blob.core.windows.net/marketing/juan-1172.jpeg
+            # Pattern to capture and replace the image block with title
+            image_pattern = re.compile(r"image:\s*\n\s*src:\s*/images/work/.*\s*\n\s*title:\s*Transform Your Online Dating Profile", re.MULTILINE)
+            new_image_block = """
+image:
+      src: /web/images/nick-before-after-1.png
+    title: Transform Your Online Dating Profile
             """
             
-            new_content = images_pattern.sub(new_images_block.strip() + '\n', content)
+            new_content = image_pattern.sub(new_image_block.strip(), content)
             
             # Write back the modified content to the file
             with open(file_path, 'w') as file:
