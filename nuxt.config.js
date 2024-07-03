4c02287da6b5c7cd478f2ebe09df36ea17610705
Adon <adon@cloudcannon.com>
2024-07-03 21:57:55 +1200

    Fix: fixing a JS error that was logging to the console

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,6 @@ export default defineNuxtConfig({
           _linkedin_partner_id = "6338732";
           window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
           window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-          </script><script type="text/javascript">
           (function(l) {
           if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
           window.lintrk.q=[]}
@@ -51,7 +50,6 @@ export default defineNuxtConfig({
           b.type = "text/javascript";b.async = true;
           b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
           s.parentNode.insertBefore(b, s);})(window.lintrk);
-          </script>
           `,
           type: "text/javascript",
           charset: "utf-8",
