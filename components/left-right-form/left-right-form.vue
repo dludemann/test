fbf06cbcff4c1615535ccb6752e23a708b34fc0e
dludemann <dludemann@aspiralabs.co>
2024-07-15 17:22:14 -0400

    h2 fixes

--- components/left-right-form/left-right-form.vue
+++ components/left-right-form/left-right-form.vue
@@ -1,6 +1,17 @@
 <script>
 export default {
   props: ["block", "dataBinding"],
+  computed: {
+    titleParts() {
+      const [_, cityState] = this.block.title.match(
+        /Professional Dating Photography\s+in\s+(.*)/
+      );
+      return {
+        main: "Professional Dating Photography",
+        cityState: cityState || "",
+      };
+    },
+  },
   mounted() {
     console.log("block", this.block);
   },
@@ -20,14 +31,21 @@ export default {
       class="container mx-auto flex justify-between items-center lg:items-start px-4 lg:px-32 flex-col lg:flex-row"
     >
       <article class="max-w-[560px] w-full text-white pr-4">
-        <h2 class="font-bold text-[2.2rem] text-[3.125rem]">
-          {{ block.title }}
-        </h2>
+        <div class="font-bold text-[2.2rem] text-[3.125rem]">
+          <h2>
+            {{ titleParts.main }}
+            <span>{{ titleParts.cityState }}</span>
+          </h2>
+        </div>
         <div class="bg-primary-500 w-[120px] h-[10px] my-4" />
         <p class="text-20px">
           {{ block.description }}
         </p>
-        <div class="flex flex-col gap-4 my-6" v-for="list_item in block.list">
+        <div
+          class="flex flex-col gap-4 my-6"
+          v-for="list_item in block.list"
+          :key="list_item.text"
+        >
           <div class="flex items-center gap-4">
             <tma-image :src="list_item.icon" class="w-[32px] h-[32px]" alt="" />
             <p class="text-[18.5px] mb-0">{{ list_item.text }}</p>
