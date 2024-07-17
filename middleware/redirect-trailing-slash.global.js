8c003988211060e27e286b3206092e63a07b2675
dludemann <dludemann@aspiralabs.co>
2024-07-17 02:19:17 -0400

    move ot middleware

--- middleware/redirect-trailing-slash.global.js
+++ middleware/redirect-trailing-slash.global.js
@@ -1,5 +1,20 @@
+import { navigateTo } from "nuxt/app";
+
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log("trigger middleware");
+
+  // Handle Referral Stuff
+  if (to.query.source) {
+    console.log("referall");
+
+    const d = new Date();
+    const source = to.query.source;
+    d.setTime(d.getTime() + 30 * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    const sourceCookie = useCookie("source-cookie");
+    sourceCookie.value = "source=" + source + ";" + expires + ";path=/";
+    return navigateTo(to.path);
+  }
 
   if (to.path !== "/" && to.path.endsWith("/")) {
     console.log("trigger if");
